5842114589c4c7dd6289c8390bead7787d25453a
linsy king <linsy_king@sjtu.edu.cn>
2023-05-20 16:13:52 +0800

    feat: check

--- source/plugins/vndb/index.mjs
+++ source/plugins/vndb/index.mjs
@@ -12,7 +12,8 @@ export default async function ({ login, q, imports, data, computed, rest, graphq
       user: config['user'],
       finished_games_limit: config['finished.games.limit'],
       playing_games_limit: config['playing.games.limit'],
-      favourite_games: config['favourite.games']
+      favourite_games: config['favourite.games'],
+      tagmax: config['tag.limit']
     }
     return await deal(imports, mydata)
   }
@@ -35,19 +36,19 @@ async function getUserData(imports, uid) {
 }
 
 
-function getTopTags(tags) {
+function getTopTags(tags, maxtag) {
   // Sort the tags by rating and return the top 3
   tags.sort((a, b) => {
     return b.rating - a.rating
   })
   let taglist = []
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < maxtag; i++) {
     taglist.push(tags[i].name)
   }
   return taglist
 }
 
-async function getRecentlyFinishedVnList(imports, uid, max) {
+async function getRecentlyFinishedVnList(imports, uid, max, maxtag) {
   const res = await imports.axios.get("https://vndb.org/" + uid + "/ulist?s=3q8w")
   const re = new RegExp("<td class=\"tc_vote\">([^ ]*?)</td>.*?<td class=\"tc_title\"><a href=\"/(.*?)\".*?title=\"(.*?)\">(.*?)</a>.*?<td class=\"tc_started\">(.*?)</td>.*?<td class=\"tc_finished\">(.*?)</td>.*?</td>", "g")
   let match = re.exec(res.data)
@@ -64,7 +65,7 @@ async function getRecentlyFinishedVnList(imports, uid, max) {
     match = re.exec(res.data)
   }
   finished_vns = finished_vns.slice(0, max)
-  await addExtraInfo(imports, finished_vns)
+  await addExtraInfo(imports, finished_vns, maxtag)
   let matchs = /Playing.*?\((.+?)\).*?Finished.*?\((.+?)\)/
   const re2 = new RegExp(matchs)
   match = re2.exec(res.data)
@@ -80,7 +81,7 @@ function toHoursAndMinutes(totalMinutes) {
 }
 
 
-async function getRecentlyPlayingVnList(imports, uid, max) {
+async function getRecentlyPlayingVnList(imports, uid, max, maxtag) {
   const res = await imports.axios.get("https://vndb.org/" + uid + "/ulist?l=1&s=3q7w")
   const re = new RegExp("<td class=\"tc_vote\">([^ ]*?)</td>.*?<td class=\"tc_title\"><a href=\"/(.*?)\".*?title=\"(.*?)\">(.*?)</a>.*?<td class=\"tc_started\">(.*?)</td>.*?<td class=\"tc_finished\">(.*?)</td>.*?</td>", "g")
   let match = re.exec(res.data)
@@ -97,11 +98,11 @@ async function getRecentlyPlayingVnList(imports, uid, max) {
     match = re.exec(res.data)
   }
   vns = vns.slice(0, max)
-  await addExtraInfo(imports, vns)
+  await addExtraInfo(imports, vns, maxtag)
   return vns
 }
 
-async function addExtraInfo(imports, vns) {
+async function addExtraInfo(imports, vns, maxtag) {
   if (vns.length == 0) {
     return
   }
@@ -120,7 +121,7 @@ async function addExtraInfo(imports, vns) {
       const dataRes = res.data.results[j]
       if (vns[i].id == dataRes.id) {
         vns[i].image = await urlToImage(imports, dataRes.image.url)
-        vns[i].tags = getTopTags(dataRes.tags)
+        vns[i].tags = getTopTags(dataRes.tags, maxtag)
         break
       }
     }
@@ -131,7 +132,7 @@ async function urlToImage(imports, url){
   return await imports.imgb64(url, {width: 64, height: 64})
 }
 
-async function getFavouriteVns(imports, vns) {
+async function getFavouriteVns(imports, vns, maxtag) {
   if (vns.length == 0) {
     return []
   }
@@ -152,7 +153,7 @@ async function getFavouriteVns(imports, vns) {
       "altname": dataRes.alttitle ? dataRes.alttitle : dataRes.title,
       "name": dataRes.title,
       "image": await urlToImage(imports, dataRes.image.url),
-      "tags": getTopTags(dataRes.tags)
+      "tags": getTopTags(dataRes.tags, maxtag)
     })
   }
   return result
@@ -161,20 +162,21 @@ async function getFavouriteVns(imports, vns) {
 async function deal(imports, data) {
   let result = {}
   try {
+    const maxtag = data.tagmax
     const uid = data.user
     if (uid == ''){
       return
     }
     const { total_time, username } = await getUserData(imports, uid)
-    const { finished_vns, playing_num, finished_num } = await getRecentlyFinishedVnList(imports, uid, data.finished_games_limit)
+    const { finished_vns, playing_num, finished_num } = await getRecentlyFinishedVnList(imports, uid, data.finished_games_limit, maxtag)
     result.player = {
       username: username,
       total_time: toHoursAndMinutes(total_time),
       finished_num: finished_num,
       playing_num: playing_num
     }
-    const playing_vns = await getRecentlyPlayingVnList(imports, uid, data.playing_games_limit)
-    const favourite_vns = await getFavouriteVns(imports, data.favourite_games)
+    const playing_vns = await getRecentlyPlayingVnList(imports, uid, data.playing_games_limit, maxtag)
+    const favourite_vns = await getFavouriteVns(imports, data.favourite_games, maxtag)
     result.finished_vns = finished_vns
     result.playing_vns = playing_vns
     result.favourite_vns = favourite_vns
